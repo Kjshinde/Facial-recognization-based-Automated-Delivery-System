16c25419b9b25dc89d38be7175980172cc85328d
Kjshinde <kjshinde19@gmail.com>
2024-02-19 13:14:45 +0530

    Initial commit

---
+++ FaceRecognition/Recognizer.py
@@ -0,0 +1,95 @@
+from bluedot import BlueDot
+from gpiozero import Robot
+from signal import pause
+
+import cv2
+import numpy as np
+import os
+import RPi.GPIO as GPIO
+import time
+
+bd = BlueDot()
+robot = Robot(left=(17,20), right=(22,21))
+
+try:
+    def move(pos):
+        if pos.top:
+            robot.forward()
+        elif pos.bottom:
+            robot.backward()
+        elif pos.left:
+            robot.left()
+        elif pos.right:
+            robot.right()
+
+    def stop():
+        robot.stop()
+
+    bd.when_pressed = move
+    bd.when_moved = move
+    bd.when_released = stop
+
+    pause()
+finally:
+    
+
+    led = 23
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(led, GPIO.OUT)
+    GPIO.output(led,0)
+    recognizer = cv2.face.LBPHFaceRecognizer_create()
+    recognizer.read('trainer.yml')
+    cascadePath = "haarcascade_frontalface_default.xml"
+    faceCascade = cv2.CascadeClassifier(cascadePath);
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    #iniciate id counter
+    id = 0
+    # names related to ids: example ==> Marcelo: id=1,  etc
+    names = ['None', 'Tanmay', 'Kshitij', 'krutyanjay'] 
+    # Initialize and start realtime video capture
+    cam = cv2.VideoCapture(0)
+    cam.set(3, 640) # set video widht
+    cam.set(4, 480) # set video height
+    # Define min window size to be recognized as a face
+    minW = 0.1*cam.get(3)
+    minH = 0.1*cam.get(4)
+    while True:
+        ret, img =cam.read()
+        img = cv2.flip(img, -1) # Flip vertically
+        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        faces = faceCascade.detectMultiScale( 
+            gray,
+            scaleFactor = 1.2,
+            minNeighbors = 5,
+            minSize = (int(minW), int(minH)),
+           )
+        for(x,y,w,h) in faces:
+            cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+            id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
+            # Check if confidence is less them 100 ==> "0" is perfect match 
+            if (confidence < 80):
+                id = names[id]
+                confidence = "{0}%".format(round(100 - confidence))
+                GPIO.output(led, 1)
+                print("Opening Lock")
+               # time.sleep(5)
+     #           GPIO.output(relay, 1)
+            else:
+                id = "unknown"
+                confidence = "  {0}%".format(round(100 - confidence))
+                GPIO.output(led, 0)
+                print("Closed Lock")
+            cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
+            cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)  
+        cv2.imshow('camera',img)
+        k = cv2.waitKey(10) & 0xff # Press 'ESC' for exiting video
+        if k == 27:
+            break
+    # Do a bit of cleanup
+    print("\n [INFO] Exiting Program and cleanup stuff")
+    cam.release()
+    cv2.destroyAllWindows()
+
+    
+    
